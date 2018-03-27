e09c1683f7a41227729a878663363d3b4e19e752
MegaFeet <36816724+MegaFeet@users.noreply.github.com>
2018-03-27 14:09:29 +0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/sfsdfafd/pen/yKPEZo.
+
+ Simple user profile widget, based on mobile view and trying to incorporate flat design.
---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+        $(document).ready(function(){
+            $('.menu').click(function(){
+                $(this).toggleClass('active');
+                $(this).parents('.container').find('.navigation').slideToggle('fast');
+            });
+            $('.actions').hover(
+                function(){
+                    $(this).find('.btn').removeClass('success').addClass('danger').text('Unfollow');
+                },
+                function(){
+                    $(this).find('.btn').removeClass('danger').addClass('success').text('Following');
+                }
+            );
+        });
